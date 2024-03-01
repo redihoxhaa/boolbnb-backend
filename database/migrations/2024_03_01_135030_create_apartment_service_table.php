397b596667f0e9ab8a7127f12fa13fc37becf709
WebGCDev <gaetano.chi@gmail.com>
2024-03-01 14:55:03 +0100

    feat: msRr Service

---
+++ database/migrations/2024_03_01_135030_create_apartment_service_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartment_service', function (Blueprint $table) {
+            $table->foreignId('apartment_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('service_id')->constrained()->cascadeOnDelete();
+            $table->primary(['apartment_id', 'service_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartment_service');
+    }
+};
