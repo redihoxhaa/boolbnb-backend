3104d669bef1fbaebf248f799c6a1635bf4cbc37
WebGCDev <gaetano.chi@gmail.com>
2024-03-01 15:06:07 +0100

    feat: msRr Sponsorship

---
+++ app/Http/Controllers/SponsorshipController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sponsorship;
+use App\Http\Requests\StoreSponsorshipRequest;
+use App\Http\Requests\UpdateSponsorshipRequest;
+
+class SponsorshipController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSponsorshipRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Sponsorship $sponsorship)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Sponsorship $sponsorship)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSponsorshipRequest $request, Sponsorship $sponsorship)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Sponsorship $sponsorship)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_03_01_135734_create_sponsorships_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sponsorships', function (Blueprint $table) {
+            $table->id();
+            $table->string('package_name');
+            $table->decimal('package_price', 3,2);
+            $table->unsignedSmallInteger('package_duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sponsorships');
+    }
+};
