4bf5e9ee8c3cb1ca97b982c61453ca726e67273f
redihoxhaa <redi.hoxha1996@gmail.com>
2024-03-01 15:34:19 +0100

    feat: sancite relazioni tra i modelli

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Apartment extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['services'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function visits()
+    {
+        return $this->hasMany(Visit::class);
+    }
+
+    public function services()
+    {
+        return $this->belongsToMany(Service::class);
+    }
+
+    public function sponsorships()
+    {
+        return $this->belongsToMany(Sponsorship::class);
+    }
 }
--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Visit extends Model
 {
     use HasFactory;
+
+    public function apartment()
+    {
+        return $this->belongsTo(Apartment::class);
+    }
 }
