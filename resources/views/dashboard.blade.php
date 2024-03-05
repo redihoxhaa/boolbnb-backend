fce4b2c74864f361e5a331575cf6bfa7a971d8a6
redihoxhaa <redi.hoxha1996@gmail.com>
2024-03-05 10:45:59 +0100

    feat: modificato testo dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,8 +4,8 @@
     <div class="welcome container">
         <div class="row justify-content-center">
             <div class="col-md-8 mt-4">
-                <h1 class="p-3">
-                    Welcome to my portfolio, {{ Auth::user()->name }}!
+                <h1 class="p-3 text-white">
+                    Welcome to your dashboard {{ Auth::user()->name }}!
                 </h1>
 
                 <a href="{{ route('admin.apartments.create') }}">Aggiungi appartamento</a>
