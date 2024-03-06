50b4e06e044f9134cb3257590deec390f3137294
redihoxhaa <redi.hoxha1996@gmail.com>
2024-03-06 10:40:01 +0100

    feat: api per visione generale appartamenti

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -10,29 +10,15 @@ class ApartmentController extends Controller
 {
     public function index(Request $request)
     {
-        // Ottieni gli appartamenti sponsorizzati ordinati per data di sponsorizzazione decrescente
-        $sponsoredApartments = Apartment::with(['sponsorships' => function ($query) {
-            $query->orderBy('created_at', 'desc')->first();
-        }])
-            ->has('sponsorships')
-            ->orderBy('sponsorships.created_at', 'desc')
+        // Eager loading per caricare le relazioni sponsorizzate
+        $apartments = Apartment::with('sponsorships')
+            ->orderByRaw('CASE 
+                WHEN id IN (SELECT apartment_id FROM apartment_sponsorship) THEN 0
+                WHEN is_visible = 1 THEN 1
+                ELSE 2
+            END')
+            ->orderByDesc('created_at')
             ->get();
-
-        // Ottieni gli appartamenti non sponsorizzati ordinati per data di creazione decrescente
-        $nonSponsoredApartments = Apartment::with('sponsorships')
-            ->whereDoesntHave('sponsorships')
-            ->orderBy('created_at', 'desc')
-            ->get();
-
-        // Ottieni gli appartamenti nascosti ordinati per data di creazione decrescente
-        $hiddenApartments = Apartment::where('is_visible', 0)
-            ->orderBy('created_at', 'desc')
-            ->get();
-
-        // Combina gli appartamenti in un'unica collezione ordinata
-        $apartments = $sponsoredApartments
-            ->merge($nonSponsoredApartments)
-            ->merge($hiddenApartments);
 
         return response()->json($apartments);
     }
