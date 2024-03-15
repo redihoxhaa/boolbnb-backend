3e2629cd9a5abbe8caee4d76b5610991d5fbb807
MirkoCherchi <mirkocherchi1992@gmail.com>
2024-03-15 13:54:43 +0100

    create-style

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-    <div class="container">
+    <div class="container create">
 
         <h1 class="mb-5">Create New Apartment</h1>
 
@@ -13,7 +13,222 @@
             @csrf <!-- Token CSRF -->
 
 
-            <div class="row mb-3">
+            <div class="row">
+                <div class="col-md-7">
+                    <h6 class="fw-bold mb-4 text-uppercase">Info</h6>
+
+                    <input placeholder="Title..." type="text" class="form-control" id="title" name="title" required
+                        maxlength="255" value="{{ old('title') }}">
+
+                    @error('title')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div> <!-- Messaggio di errore -->
+                    @enderror
+                    <textarea class="form-control mt-3" placeholder="Describe your apartament...." id="description" name="description"
+                        required>{{ old('description') }}</textarea>
+
+                    @error('description')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div> <!-- Messaggio di errore -->
+                    @enderror
+
+                    <div class="row mt-3">
+                        <div class="p-2 col-3">
+                            <div class=" text-center card-icon">
+                                <div>(i) Rooms</div>
+                                <div class="room-counter mt-3">
+                                    <div class="room-control">
+                                        -
+                                    </div>
+                                    <div class="room-number">2</div>
+                                    <div class="room-control">
+                                        +
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="p-2 col-3">
+                            <div class=" text-center card-icon">
+                                <div>(i) Rooms</div>
+                                <div class="room-counter mt-3">
+                                    <div class="room-control">
+                                        -
+                                    </div>
+                                    <div class="room-number">2</div>
+                                    <div class="room-control">
+                                        +
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-2 col-3">
+                            <div class=" text-center card-icon">
+                                <div>(i) Rooms</div>
+                                <div class="room-counter mt-3">
+                                    <div class="room-control">
+                                        -
+                                    </div>
+                                    <div class="room-number">2</div>
+                                    <div class="room-control">
+                                        +
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-2 col-3">
+                            <div class=" text-center card-icon">
+                                <div>(i) Rooms</div>
+                                <div class="room-counter mt-3">
+                                    <div class="room-control">
+                                        -
+                                    </div>
+                                    <div class="room-number">2</div>
+                                    <div class="room-control">
+                                        +
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-6">
+                            <h5 class="fw-bold">Location</h5>
+                            <div class="input-container">
+                                <input type="text" class="form-control" id="address" name="address" autocomplete="off"
+                                    placeholder="Select address..." value="{{ old('address') }}">
+                                <i class="fas fa-search"></i>
+                            </div>
+                            <div class="mt-3">
+                                <img class="w-100" src="{{ asset('assets/images/Group256.png') }}"" alt="img">
+                            </div>
+
+                            @error('address')
+                                <div class="alert alert-danger mt-2">{{ $message }}</div>
+                            @enderror
+
+                            <div id="suggestionsMenu" class="card position-absolute w-100 radius d-none">
+                                <ul class="suggestions-list"></ul>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="d-flex align-items-center justify-content-between">
+                                <h5 class="fw-bold">Service</h5>
+                                <span>18 services selected </span>
+                            </div>
+                            <div class="input-container">
+                                <input type="text" class="form-control" id="service" name="service" autocomplete="off"
+                                    placeholder="Search for service..." value="{{ old('service') }}">
+                                <i class="fas fa-search"></i>
+                            </div>
+                            <div class="mt-3">
+                                <ul>
+                                    <li>
+                                        @foreach ($services as $key => $service)
+                                            <label class="service-label service-list m-1{{ $key >= 10 ? ' extra' : '' }}"
+                                                for="service{{ $service->id }}">
+                                                <input type="checkbox" id="service{{ $service->id }}" name="services[]"
+                                                    value="{{ $service->id }}" style="display: none;">
+                                                <span class="checkmark"></span>
+                                                <span>{{ $service->name }}</span>
+                                            </label>
+                                        @endforeach
+                                        <div class="show-more mt-3 ms-2 text-decoration-underline">Show More</div>
+                                    </li>
+                                </ul>
+                                @error('services')
+                                    <div class="alert alert-danger mt-2">{{ $message }}</div> <!-- Messaggio di errore -->
+                                @enderror
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-5">
+                    <div>
+                        <h6 class="fw-bold mb-4 text-uppercase">Photo</h6>
+                        <label class="custum-file-upload" for="file">
+                            <div class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24">
+                                    <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                                    <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g>
+                                    <g id="SVGRepo_iconCarrier">
+                                        <path fill=""
+                                            d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z"
+                                            clip-rule="evenodd" fill-rule="evenodd"></path>
+                                    </g>
+                                </svg>
+                            </div>
+                            <div class="text">
+                                <span>Click to upload image</span>
+                            </div>
+                            <input type="file" id="file">
+                        </label>
+                        <div class="d-flex gap-2">
+                            <div class="add-photo">+</div>
+                            <div class="add-text">
+                                <span class="suggestion">(i) suggestion</span>
+                                <div class="d-flex align-items-center gap-3">
+                                    <i>(i)</i>
+                                    <p class="p-more-photo m-0">Add more photos, you'll assist the customer in making a
+                                        better-informed decision.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h5 class="fw-bold mt-5 mb-3 text-uppercase">Select your sponsorship</h5>
+                        <div class="row">
+                            <div class="p-2 col-3">
+                                <div class=" text-center card-icon">
+                                    <div>(i) Rooms</div>
+                                    <div class="room-counter mt-3">
+                                        <div class="room-control">
+                                            -
+                                        </div>
+                                        <div class="room-number">2</div>
+                                        <div class="room-control">
+                                            +
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="p-2 col-3">
+                                <div class=" text-center card-icon">
+                                    <div>(i) Rooms</div>
+                                    <div class="room-counter mt-3">
+                                        <div class="room-control">
+                                            -
+                                        </div>
+                                        <div class="room-number">2</div>
+                                        <div class="room-control">
+                                            +
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-2 col-3">
+                                <div class=" text-center card-icon">
+                                    <div>(i) Rooms</div>
+                                    <div class="room-counter mt-3">
+                                        <div class="room-control">
+                                            -
+                                        </div>
+                                        <div class="room-number">2</div>
+                                        <div class="room-control">
+                                            +
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            {{-- <div class="row mb-3">
 
                 <!-- Campo per il titolo dell'appartamento, con validazione e ripristino dei dati precedenti in caso di errore -->
                 <div class="col-md-6">
@@ -177,7 +392,7 @@
             </div>
 
             <!-- Bottone di submit -->
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Submit</button> --}}
         </form>
     </div>
 
@@ -220,6 +435,25 @@
                 })
                 .catch(error => console.error('Error:', error));
         }
+        document.addEventListener('DOMContentLoaded', function() {
+            const showMoreBtn = document.querySelector('.show-more');
+            const extraServices = document.querySelectorAll('.service-list.extra');
+
+            // Nascondi gli elementi extra inizialmente
+            extraServices.forEach(service => {
+                service.style.display = 'none';
+            });
+
+            // Aggiungi un gestore per il clic sul pulsante "Show More"
+            showMoreBtn.addEventListener('click', function() {
+                extraServices.forEach(service => {
+                    service.style.display = 'inline-block';
+                });
+                // Nascondi il pulsante "Show More" dopo il clic
+                this.style.display = 'none';
+            });
+        });
+
 
 
         // Funzione per gestire il cambiamento del file di input e visualizzare le anteprime delle immagini
