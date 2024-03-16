2ff9aaa72dbb822d9a43fbc4296ab93a4fd0353a
lordevito <lollo.devito@hotmail.com>
2024-03-16 12:26:33 +0100

    feat: pagina sponsorships da fixare

--- resources/views/admin/apartments/sponsorship.blade.php
+++ resources/views/admin/apartments/sponsorship.blade.php
@@ -5,31 +5,82 @@
 @section('content')
 
 
-    <div class="container py-3">
 
-        <div class="text-center mb-5">Buy a sponsorship for {{ $apartment->title }}</div>
 
+    {{-- <link rel="stylesheet" href="{{ asset('scss/partials/sponsorships-style.scss') }}"> --}}
+    <div class="container py-4 sponsorship">
+        <div>
+            <span class="text-secondary">Admin</span>
+            <span class="text-secondary">/</span>
+            <span class="text-secondary">Apartments</span>
+            <span class="text-secondary">/</span>
+            <span>Sponsorships</span>
+        </div>
+        <div class="mt-5 mb-5 d-flex align-items">
+            <h1>Sponsorship</h1>
+        </div>
         <form id="payment-form" action="{{ route('admin.apartments.buySponsorship', $apartment->id) }}" method="POST">
             @csrf
-            <div class="row radio-input gap-5 justify-content-center">
+            <div class="row gap-3">
+                <div class="d-flex col-4 radio-input gap-3 justify-content-center flex-column">
+                    <h6 class="text-black">SELECT YOUR PLAN</h6>
                 @foreach ($sponsorships as $sponsorship)
                     <input value="{{ $sponsorship->id }}" name="sponsorship_choice" id="sponsorship{{ $sponsorship->id }}"
                         type="radio" class="sponsorship-radio" required>
-                    <label for="sponsorship{{ $sponsorship->id }}" class="col-3 d-flex flex-column align-items-center">
-                        <span>{{ $sponsorship->package_name }}</span>
-                        <span>€{{ $sponsorship->package_price }}</span>
-                        <span>{{ $sponsorship->package_duration }}h</span>
+                    <label for="sponsorship{{ $sponsorship->id }}" class="row ">
+                        <div class="col-3">
+                            <span>icona</span>
+                        </div>
+                        <div class="col-9 d-flex flex-column">
+                            <span>{{ $sponsorship->package_name }}</span>
+                            <span>{{ $sponsorship->package_duration }}h</span>
+                            <span class="align-self-end">€{{ $sponsorship->package_price }}</span>
+                        </div>
                     </label>
                 @endforeach
+                </div>
+                <div class="col-7 ps-4 mt-2">
+                    <h6 class="text-black">PAYMENT</h6>
+                    <div class="row mt-3">
+                        <div class="col-3">
+                            <div>
+                                @if ($apartment->images)
+                                    <img class="apartment-img-sponsorships"
+                                        src="{{ asset('storage/' . explode(',', $apartment->images)[0]) }}"
+                                        alt="apartment-image">
+                                @else
+                                    <img class="apartment-img-sponsorships"
+                                        src="https://saterdesign.com/cdn/shop/products/property-placeholder_a9ec7710-1f1e-4654-9893-28c34e3b6399_600x.jpg?v=1500393334"
+                                        alt="apartment-image">
+                                @endif
+                            </div>
+                        </div>
+                        <div class="col-9">
+                            <h6>{{ $sponsorship->package_name }}</h6>
+                            <h4>{{ $apartment->title }}</h4>
+                        </div>
+                    </div>
+                    <div id="dropin-container" style="display:none;"></div>
+                    
+                </div>
             </div>
-            <div id="dropin-container" style="display:none;"></div>
-            <button id="submit-button" class="buttonPayment buttonPayment--small buttonPayment--green"
-                style="display:none;">Purchase</button>
+            <div class="row pe-5"> 
+                <div class="mt-5 pe-5">
+                    <div class="d-flex flex-row-reverse">
+                        <h2 class="strong">€{{ $sponsorship->package_price }}</h2>
+                        <a href="{{ route('admin.apartments.index') }}" class="btn btn-secondary btn-sm mb-4 "><i
+                        class="fas fa-arrow-left"></i> Go Back</a>
+                    </div>
+                </div>
+                <div class="mt-4 pe-5">
+                    <div class="d-flex flex-row-reverse">
+                        <button id="submit-button" class="buttonPayment buttonPayment--small buttonPayment--green"
+                        style="display:none;">Pay Now</button>
+                    </div>
+                </div>
+            </div>
         </form>
-        <div class="mt-3 text-center">
-            <a href="{{ route('admin.apartments.index') }}" class="btn btn-secondary btn-sm mb-4"><i
-                    class="fas fa-arrow-left"></i> Go Back</a>
-        </div>
+        
 
         <div>
             <div>CART DEBUG</div>
