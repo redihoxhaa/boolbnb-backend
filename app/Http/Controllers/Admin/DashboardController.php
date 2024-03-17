d827b2af5c15756b5d995cd492177022614593a7
redihoxhaa <redi.hoxha1996@gmail.com>
2024-03-17 22:30:28 +0100

    feat: rotta per dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Apartment;
+use App\Models\Message;
+use App\Models\Visit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+
+        $userId = auth()->id();
+        $apartments = Apartment::where('user_id', $userId)->get();
+        $totalApartments = count($apartments);
+        $totalMessages = $apartments->messages()->count();
+        $totalVisits = $apartments->visits()->count();
+
+        return view('dashboard', compact('totalApartments', 'totalMessages', 'totalVisits'));
     }
 }
