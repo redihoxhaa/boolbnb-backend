ed482bd40f31f4cf1b81eb9557d180b8936a992d
redihoxhaa <redi.hoxha1996@gmail.com>
2024-03-18 18:07:27 +0100

    feat: modificati seeder

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Message;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -15,20 +16,23 @@ class MessageSeeder extends Seeder
      */
     public function run(): void
     {
-        // Temporarily disable foreign key checks
-        Schema::disableForeignKeyConstraints();
-
-        // Truncate the apartments table
         Message::truncate();
-
-        // Re-enable foreign key checks
-        Schema::enableForeignKeyConstraints();
-
 
         $messages = include base_path('data/messages.php');
 
         foreach ($messages as $messageData) {
-            Message::create($messageData);
+            $message = new Message();
+            $message->sender_name = $messageData['sender_name'];
+            $message->sender_email = $messageData['sender_email'];
+            $message->message_text = $messageData['message_text'];
+
+            // Genera una data casuale compresa tra il 10 e il 18 marzo 2024 con orario
+            $createdAt = Carbon::createFromFormat('Y-m-d H:i:s', '2024-03-10 00:00:00')
+                ->addSeconds(rand(0, 691199)) // 691199 secondi sono esattamente 7 giorni meno un secondo
+                ->format('Y-m-d H:i:s');
+
+            $message->created_at = $createdAt;
+            $message->save();
         }
     }
 }
--- database/seeders/VisitSeeder.php
+++ database/seeders/VisitSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Visit;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
@@ -21,7 +22,17 @@ class VisitSeeder extends Seeder
         $visits = include base_path('data/visits.php');
 
         foreach ($visits as $visitData) {
-            Visit::create($visitData);
+            $visit = new Visit();
+            $visit->apartment_id = $visitData['apartment_id'];
+            $visit->visitor_ip = $visitData['visitor_ip'];
+
+            // Genera una data casuale compresa tra il 10 e il 18 marzo 2024 con orario
+            $createdAt = Carbon::createFromFormat('Y-m-d H:i:s', '2024-03-10 00:00:00')
+                ->addSeconds(rand(0, 691199)) // 691199 secondi sono esattamente 7 giorni meno un secondo
+                ->format('Y-m-d H:i:s');
+
+            $visit->created_at = $createdAt;
+            $visit->save();
         }
     }
 }
